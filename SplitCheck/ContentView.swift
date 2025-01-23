c0e5de51fc508f139a23ccf031ca7100ece42b92
HABEN-IOS <hgebr00@fiu.edu>
2025-01-23 11:07:14 -0500

    Add defualt tip of %20

--- SplitCheck/ContentView.swift
+++ SplitCheck/ContentView.swift
@@ -45,7 +45,7 @@ struct ContentView: View {
                     .pickerStyle(.navigationLink)
                 }
                 
-                Section("How much do you want to tip?") {
+                Section("How much do you want to tip? (%20)") {
                     Picker("Tip Percentage", selection: $tipPercentage) {
                         ForEach(0..<101) {
                             Text($0, format: .percent)
