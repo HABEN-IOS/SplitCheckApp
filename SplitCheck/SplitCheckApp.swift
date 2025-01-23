4c443fd6dd4f46ceb1520e526de4a0df6e6aa65c
Haben Gebremeskel <haben.1@icloud.com>
2025-01-23 10:20:05 -0500

    Initial Commit

---
+++ SplitCheck/SplitCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitCheckApp.swift
+//  SplitCheck
+//
+//  Created by Haben Gebremeskel on 1/23/25.
+//
+
+import SwiftUI
+
+@main
+struct SplitCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
